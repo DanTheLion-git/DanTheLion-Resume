3e8852e6746e101c64169dbc788885b0f59f7084
Danthelion <info.mvdvl@gmail.com>
2025-11-26 12:33:45 +0100

    Re-adjusted the resume to make use of Wordpress blogging instead of the whole Netlify setup

--- Astro-Resume/astro.config.mjs
+++ Astro-Resume/astro.config.mjs
@@ -1,7 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  // You can fill this in after first deploy if you don't know it yet
-  site: 'https://danthelion-resume.netlify.app',
-  base: '/', // or just remove `base` entirely
-});
+// https://astro.build/config
+export default defineConfig({});
